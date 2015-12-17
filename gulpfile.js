4fff6b82b8daac1298d900c26b6b6bd3498487c7
kushdilip <kush.dilip@gmail.com>
2015-12-18 00:13:03 +0530

    minimalistic redesign of whole website using purecss, font-awesome. tools used bower, npm, and gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,20 @@ var minifyCSS = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
 
+gulp.task('dist', ['css', 'font']);
+
 gulp.task('css', function  () {
   gulp.src([
-          'bower_components/**/normalize-css/normalize.css',
           'bower_components/**/pure/pure.css',
+          'bower_components/**/pure/grids-responsive.css',
+          'vendor/**/font-awesome/css/font-awesome.css',
+          'vendor/**/css/style.css'
         ])
     .pipe(minifyCSS())
     .pipe(concat('main.css'))
     .pipe(gulp.dest('dist/css'))
 })
 
-    
+gulp.task('font', function() {
+  gulp.src('vendor/font-awesome/fonts/**/*').pipe(gulp.dest('dist/fonts'))
+})
